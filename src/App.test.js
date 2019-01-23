d377325fb129c92563235baef99282a95a03481a
Mike Nguyen <nguyenduchung0609@gmail.com>
2019-01-23 12:16:53 +0800

    test 4

--- src/App.test.js
+++ src/App.test.js
@@ -27,3 +27,9 @@ it('renders without crashing 3', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing 3', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
