f72253d15650c8a261e726aa08afa1381ab2c9b5
Mike Nguyen <nguyenduchung0609@gmail.com>
2019-02-09 05:06:08 +0800

    test

--- src/components/__tests__/List.test.js
+++ src/components/__tests__/List.test.js
@@ -32,6 +32,20 @@ describe('List tests', () => {
     expect(wrapper.find('.list-items')).toBeDefined();
     expect(wrapper.find('.item')).toHaveLength(items.length);
   });
+
+  it('renders list-items', () => {
+    const items = ['one', 'two', 'three'];
+
+    // Replace shallow iwth mount
+    const wrapper = mount(<List items={items} />);
+
+    // Let's check what wrong in our instance
+    console.log(wrapper.debug());
+
+    // Expect the wrapper object to be defined
+    expect(wrapper.find('.list-items')).toBeDefined();
+    expect(wrapper.find('.item')).toHaveLength(items.length);
+  });
 });
 
 //https://scotch.io/tutorials/testing-react-components-with-enzyme-and-jest
